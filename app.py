dde369765a929900ec2929c76bef20ff5bff37b5
NigelGoh16 <nigell.goh@gmail.com>
2023-11-21 17:18:25 +0800

    Designed the homepage. Added a css file and organized code much more neatly. Added a reset button to the analysis pages. Modified the texts in the analysis pages. Adjusted the spacing of the word-cloud image. Temporarily removed the Analysis History Nav on the sidebar.

--- app.py
+++ app.py
@@ -15,44 +15,32 @@ from functions import simple_prediction, custom_prediction, word_cloud, word_tab
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 server = app.server
 
-# styling the sidebar
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-    "z-index": "8000"}
+sidebar = html.Div(children=[
+    html.Img(src=app.get_asset_url('Profile pic-01.png'), width='100%', style={'border-radius': '50%'}),
+    html.P("Nigel Llaneta Goh", className="lead text-center"),
+    html.Div(children=[
+        html.A(href="https://www.linkedin.com/in/nigel-goh-a9593a20a/", children=[html.Img(src=app.get_asset_url('linkedin.png'), width='25px')], target='_blank', style={'margin-right':'5px'}), 
+        html.A(href="https://github.com/NigelGoh16", children=[html.Img(src=app.get_asset_url('github-01.jpg'), width='25px', style={'border-radius': '50%'})], target='_blank', style={'margin-left':'5px'})],
+        className='d-flex justify-content-center'),
+    html.Hr(),
+    html.P("Analysis Section", className="lead"),
+    dbc.Nav([
+        dbc.NavLink("Home", href="/", active="exact", className="nav-links"),
+        dbc.NavLink("Simple Analysis", href="/simple-analysis", active="exact", className="nav-links"),
+        dbc.NavLink("Custom Analysis", href="/custom-analysis", active="exact", className="nav-links"),
+        dbc.NavLink("YouTube Analysis", href="/youtube-analysis", active="exact", className="nav-links")
+        ], vertical=True, pills=True),
+    # html.Hr(),
+    # html.P("Analysis Data History", className="lead"),
+    # dbc.Nav([
+    #     dbc.NavLink("Simple Analysis", href="/page-4", active="exact", disabled=True),
+    #     dbc.NavLink("Custom Analysis", href="/page-5", active="exact", disabled=True),
+    #     dbc.NavLink("Youtube Analysis", href="/page-6", active="exact", disabled=True),
+    # ], vertical=True, pills=True)
+    ],
+    className="div-side")# overflow-scroll")
 
-# padding for the page content
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",}
-
-sidebar = html.Div([
-        html.H2("Sentiment Analysis", className="display-5"),
-        html.Hr(),
-        html.P("Analysis Section", className="lead"),
-        dbc.Nav([
-            dbc.NavLink("Home", href="/", active="exact"),
-            dbc.NavLink("Simple Analysis", href="/page-1", active="exact"),
-            dbc.NavLink("Custom Analysis", href="/page-2", active="exact"),
-            dbc.NavLink("Youtube Analysis", href="/page-3", active="exact")
-            ], vertical=True, pills=True),
-        html.Hr(),
-        html.Br(),
-        html.P("Analysis Data History", className="lead"),
-        dbc.Nav([
-            dbc.NavLink("Simple Analysis", href="/page-4", active="exact", disabled=True),
-            dbc.NavLink("Custom Analysis", href="/page-5", active="exact", disabled=True),
-            dbc.NavLink("Youtube Analysis", href="/page-6", active="exact", disabled=True),
-        ], vertical=True, pills=True)],
-    style=SIDEBAR_STYLE,)
-
-content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+content = html.Div(id="page-content", children=[], className="div-content")
 
 def table_style(df, size=15, tooltip=0, row_selectable=False, row_deletable=False):
     if tooltip==0:
@@ -84,34 +72,73 @@ def table_style(df, size=15, tooltip=0, row_selectable=False, row_deletable=Fals
         )
     return table
 
-def general_page(title, desc, placeholder="", button_text="", type=0):
+def general_page(path="", title="", desc="", text="", help="", placeholder="", button_text="", type=0):
     if type == 0:
         page =  [
-                html.H1(title, style={'textAlign':'center'}),
-                html.P(desc, className="lead", style={'textAlign':'center'}),
-                html.Div(className='mx-auto d-grid col-6 p-2', children=[
-                    dcc.Input(id='text', type='text', placeholder=placeholder)]),
-                html.Br(),
+                html.H1(title, className='text-center bebas pt-5'),
+                html.P(desc, className="lead text-center"),
+                html.P(text, className="lead text-center"),
+                html.P(help, className="lead text-center h6 text-muted"),
+                html.Div(className='mx-auto d-grid col-6', children=[
+                    dcc.Input(id='text', type='text', placeholder=placeholder)]), html.Br(),
                 html.Div(className='d-flex justify-content-center', children=[
-                    html.Button(button_text, id='button')]),
-                html.Br(),
-                html.Div(id='output', style={'textAlign':'center'})
+                    html.Div([html.A(children=[html.Button(id='reset', children='Reset', className='btn btn-outline-danger')], href=path)], className='px-4'),
+                    html.Div([html.Button(button_text, id='button', className='btn btn-primary')], className='px-4')]), html.Br(),
+                html.Div(id='output', className='text-center')
                 ]
     elif type == 1:
         page =  [
-                html.H1(title, style={'textAlign':'center'}),
-                html.P(desc, className="lead", style={'textAlign':'center'}),
+                html.H1(title, className='text-center bebas pt-5'),
+                html.P(desc, className="lead text-center p-1"),
+                html.P(text, className="lead text-center"),
+                html.P(help, className="lead text-center text-1"),
                 dcc.Upload(id='upload-data', 
                     className='d-flex justify-content-center mx-auto  d-grid col-6 p-4  bg-light border border-dashed rounded-3', 
                     children=html.Div(['Drag and Drop or ', html.A('Select Files')]), 
                     multiple=True,),
                 html.Div(id='output-datatable')
                 ]
+    elif type == 2:
+        page = [        
+                html.Div(children=[
+                    html.Div(children=[
+                        html.Img(src=app.get_asset_url('Word gif.gif'))
+                    ], style={'display': 'flex', 'flex-direction': 'column', 'width': '100%'}),
+                    html.Div(children=[
+                        html.H1('Uncover the Emotional Pulse of the Web with', className='text-center base-light bebas'),
+                        html.H1('Sentiment Analytics', className='text-center base-light bebas'),
+                        html.P("Delve into the world of sentiment analysis and uncover the hidden emotions that drive online conversations. Our powerful web app empowers you to effortlessly analyze text, process custom files, and even extract insights from YouTube comment data.", className="lead text-center base-light"),
+                        html.Br()
+                    ], style={'position': 'absolute','top': '5px', 'padding-top': '10px', 'width': '83%', 'z-index': 1, 'background-color': '#212529', 'opacity': 0.9}),
+                    html.Div(children=[
+                        html.Div(children=[
+                            html.Div(children=[
+                                html.H1('Simple Analysis', className='text-center bebas'),
+                                html.P('Instantly gauge the sentiment of any text snippet, from a sentence to a whole paragraph.', className='text-center'),
+                                html.Div(className='d-flex justify-content-center', children=[
+                                    html.A(children=[html.Button(id='simple-dir', children='Try Now!', className='btn btn-outline-light')], href="/simple-analysis")])
+                            ], className='col-md-4 border div-home-dir'),
+                            html.Div(children=[
+                                html.H1('Custom Analysis', className='text-center bebas'),
+                                html.P('Upload and analyze your own text files to gain a deeper understanding of their emotional content.', className='text-center'),
+                                html.Div(className='d-flex justify-content-center', children=[
+                                    html.A(children=[html.Button(id='custom-dir', children='Try Now!', className='btn btn-outline-light')], href="/custom-analysis")])
+                            ], className='col-md-4 border div-home-dir'),
+                            html.Div(children=[
+                                html.H1('YouTube Analysis', className='text-center bebas'),
+                                html.P('Tap into the vast trove of YouTube comments and uncover the underlying sentiment of online discussions.', className='text-center'),
+                                html.Div(className='d-flex justify-content-center', children=[
+                                    html.A(children=[html.Button(id='yt-dir', children='Try Now!', className='btn btn-outline-light')], href="/youtube-analysis")])
+                            ], className='col-md-4 border div-home-dir'),
+                        ], className='d-flex justify-content-between')
+                    ], style={'position': 'absolute', 'width': '83%', 'z-index': 1, 'bottom': '10px'})
+                ])
+                ]
     return page
 
 app.layout = html.Div([dcc.Location(id="url"), 
                        sidebar, 
-                       content])
+                       content], className="body")
 
 @app.callback(
     Output("page-content", "children", allow_duplicate=True),
@@ -121,16 +148,16 @@ app.layout = html.Div([dcc.Location(id="url"),
 def render_page_content(pathname):
     general = []
     if pathname == "/":
-        return [html.H1('Sentiment Data Analysis', style={'textAlign':'center'})]
-    elif pathname == "/page-1":
-        title, desc, placeholder, button_text = "Simple Analysis", "A simple text-based sentiment analysis", "Enter your text here", "Predict"
-        return general_page(title, desc, placeholder, button_text)
-    elif pathname == "/page-2":
-        title, desc = "Custom Analysis", "A custom file-based sentiment analysis"
-        return general_page(title, desc, type=1)
-    elif pathname == "/page-3":
-        title, desc, placeholder, button_text = "YouTube Analysis", "A YouTube-based sentiment analysis", "  Search for youtube videos with specific terms (For multiple terms, split it with \',\') ", "Search"
-        return general_page(title, desc, placeholder, button_text)
+        return general_page(type=2)
+    elif pathname == "/simple-analysis":
+        path, title, desc, text, help, placeholder, button_text = "/simple-analysis", "Simple Analysis", "Explore text sentiment in an instant! Whether it's a sentence or a paragraph, decode its sentiment in a click.", "Discover now!", "Enter or paste the text to analyse below", "Enter text", "Predict"
+        return general_page(path, title, desc, text, help, placeholder, button_text)
+    elif pathname == "/custom-analysis":
+        path, title, desc, text = "/custom-analysis", "Custom Analysis", "Click or drag-and-drop your CSV or Excel files below to unlock a deeper insight into the emotional content of your texts.", "Dive into personalized text analysis now!"
+        return general_page(path, title, desc, text, type=1)
+    elif pathname == "/youtube-analysis":
+        path, title, desc, text, help, placeholder, button_text = "/youtube-analysis", "YouTube Analysis", "Uncover sentiment from YouTube discussions! Dive into the realm of YouTube comments and decode their underlying sentiment in a tap.", "Explore now!", "Search for YouTube videos with specific terms. For multiple terms, split it with \",\"", "Search Terms", "Search"
+        return general_page(path, title, desc, text, help, placeholder, button_text)
 
 @app.callback(Output('output-datatable', 'children'),
               [Input('upload-data', 'contents'),
@@ -162,13 +189,10 @@ def parse_contents(contents, filename, date):
         html.H6(datetime.datetime.fromtimestamp(date)),
         html.P("Insert data column"),
         dcc.Dropdown(id='column-data',
-                    options=[{'label':x, 'value':x} for x in df.columns]),
-        html.Br(), html.Br(),
-        html.Button(id="predict-button", children="Create Predictions"),
-        html.Hr(),
+                    options=[{'label':x, 'value':x} for x in df.columns]), html.Br(), html.Br(),
+        html.Button(id="predict-button", children="Create Predictions", className='btn btn-primary'), html.Hr(),
         table_style(df),
-        dcc.Store(id='stored-file', data=df.to_dict('records'), storage_type='session'),
-        html.Hr(), 
+        dcc.Store(id='stored-file', data=df.to_dict('records')), html.Hr(), 
     ])
 
 @app.callback(
@@ -180,22 +204,21 @@ def page_buttons(btn, url, text):
     if btn is None:
         return dash.no_update
     else:
-        if url == '/page-1':
+        if url == '/simple-analysis':
             return simple_prediction(text)
-        elif url == '/page-3':
+        elif url == '/youtube-analysis':
             terms_list = text.split(',')
             videos_df = yt_videos(terms_list)
             return dbc.Container([
                 dbc.Row([
-                    dbc.Col(table_style(videos_df, row_selectable='multi'), md=12)]),
+                    dbc.Col(table_style(videos_df, row_selectable='multi'), md=12), html.Br()]), 
                 dbc.Row([
-                    html.Div(className='d-flex justify-content-center', children=[
-                        html.Button('Add Video', id='video-button')]),
+                    html.Div(className='d-flex justify-content-center pt-3', children=[
+                        html.Button('Add Video', id='video-button', className='btn btn-primary')]),
                 dbc.Row([
                     html.Div(id='video-df', children=[])]),
-                dcc.Store(id='stored-video-df', data=videos_df.to_dict('records'), storage_type='session')
-            ])])
-
+                dcc.Store(id='stored-video-df', data=videos_df.to_dict('records'))])])
+ 
 @app.callback(
     Output('output', 'children',allow_duplicate=True),
     [Input('datatable-interact', 'selected_rows'),
@@ -213,8 +236,9 @@ def video_data(all_rows, video_dict, btn):
             dbc.Row([
                 dbc.Col(table_style(comments_df:=yt_comments(filtered_df, order='relevance')), md=12)]),
             dbc.Row([
-                html.Div(className='d-flex justify-content-center', children=[html.Button('Predict', id='yt-predict')])]),
-            dcc.Store(id='stored-comment-df', data=comments_df.to_dict('records'), storage_type='session')])
+                html.Div(className='d-flex justify-content-center', children=[html.Button('Predict', id='yt-predict', className='btn btn-primary')])], className='pb-4'),
+            dcc.Store(id='stored-comment-df', data=comments_df.to_dict('records')),
+            ])
 
 @app.callback(
     Output('page-content','children',allow_duplicate=True),
@@ -227,7 +251,7 @@ def youtube_preds(n, data):
     else:
         fig1 = px.bar(new_data := custom_prediction(df := pd.DataFrame(data), x_data:="Comment"), x=new_data['Predictions'].unique().tolist(), y=new_data['Predictions'].value_counts().tolist())
         fig1.update_layout(title='Sentiment Counts', xaxis_title='Sentiment', yaxis_title='Count', titlefont=dict(family='Arial', size=20, color='black'), width=600, height=500)
-        fig2 = px.scatter(custom_prediction(df, x_data:="Comment", type=1).groupby(['Date', 'Video_Title']), x='Date', y='Average_Predictions', color='Video_Title', size='Number_Of_Comments')
+        fig2 = px.scatter(custom_prediction(df, x_data:="Comment", type=1).groupby(['Date', 'Video_Title'], as_index=False).agg(Average_Predictions=('Predictions', 'mean'), Number_Of_Comments=('Predictions', 'count')), x='Date', y='Average_Predictions', color='Video_Title', size='Number_Of_Comments')
         fig2.update_layout(title='Sentiment Over Time', yaxis_title='Average Sentiment Polarity', titlefont=dict(family='Arial', size=20, color='black'))
         image_data, word_data = word_cloud(new_data, x_data), word_table(new_data, x_data)
         return dbc.Container([
@@ -235,14 +259,14 @@ def youtube_preds(n, data):
                 dbc.Col(dcc.Graph(figure=fig1), md=6),
                 dbc.Col(dbc.Card([
                     dbc.CardHeader("Sentiment Word Cloud", style={'font-family': 'Arial', 'font-size': '20px', 'color': 'black', 'background-color':'white'}),
-                    dbc.CardBody(html.Img(src="data:image/png;base64," + image_data))], style={'border-color':'white', 'padding-top': 15}), md=6),]),
+                    dbc.CardBody(html.Img(src="data:image/png;base64," + image_data))], style={'border-color':'white'}), md=6),]),
             dbc.Row([
                 dbc.Col(dcc.Graph(figure=fig2), md=12)]),
             dbc.Row([
                 dbc.Col(table_style(new_data[[x_data, 'Predictions']]), md=6),
-                dbc.Col(table_style(word_data, tooltip=1), md=6)
-            ])
-        ])
+                dbc.Col(table_style(word_data, tooltip=1), md=6)]),
+            dbc.Row([
+                html.Div(children=[html.A(children=[html.Button(id='yt-back', children='Reset', className='btn btn-outline-danger')], href="/youtube-analysis")], className='d-flex justify-content-center pt-2 pb-4')])])
 
 @app.callback(
     Output('page-content','children',allow_duplicate=True),
@@ -261,10 +285,12 @@ def custom_preds(n, data, x_data):
                 dbc.Col(dcc.Graph(figure=fig1), md=6),
                 dbc.Col(dbc.Card([
                     dbc.CardHeader("Sentiment Word Cloud", style={'font-family': 'Arial', 'font-size': '20px', 'color': 'black', 'background-color':'white'}),
-                    dbc.CardBody(html.Img(src="data:image/png;base64," + word_cloud(new_data, x_data)))], style={'border-color':'white', 'padding-top': 15}), md=6),]),
+                    dbc.CardBody(html.Img(src="data:image/png;base64," + word_cloud(new_data, x_data)))], style={'border-color':'white'}), md=6),]),
             dbc.Row([
                 dbc.Col(table_style(new_data[[x_data, 'Predictions']]), md=6),
-                dbc.Col(table_style(word_table(new_data, x_data), tooltip=1), md=6)])])
+                dbc.Col(table_style(word_table(new_data, x_data), tooltip=1), md=6)]),
+            dbc.Row([
+                html.A(children=[html.Button(id='custom-back', children='Reset', className='btn btn-primary')], href="/custom-analysis"), html.Br()], className='d-flex justify-content-center pb-4')])
 
 if __name__=='__main__':
-    app.run_server(debug=False, host="0.0.0.0", port=8080)
+    app.run_server(debug=True, host="0.0.0.0", port=8080)
